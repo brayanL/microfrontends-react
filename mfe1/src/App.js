46b13f36dbc4ca0a7a0f1c613383aea66b78f14a
brayanloayza <godsarmybl@gmail.com>
2021-09-22 10:30:23 -0500

    Initial Commit

---
+++ mfe1/src/App.js
@@ -0,0 +1,15 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <h1>MFE1</h1>
+      <Button>
+        {' '}
+        MFE1 Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
